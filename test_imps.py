575ee09c55e9f9e6d2ba94b1d96df9e3ba075d22
Roy Wilson <royawilson@hotmail.co.uk>
2025-04-15 20:55:59 +0100

    Sort out No Attribute is_active

---
+++ test_imps.py
@@ -0,0 +1,13 @@
+from datetime import date
+from functools import wraps
+from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from werkzeug.security import generate_password_hash, check_password_hash
+# Import your forms from the forms.py
+from forms import CreatePostForm
